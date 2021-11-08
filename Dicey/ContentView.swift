0ee8bb64a148ba693200e18241def955afefa7ff
Awhite246 <awhite@bsd220.org>
2021-11-08 10:45:04 -0600

    did stuff

--- Dicey/ContentView.swift
+++ Dicey/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
+    @State private var randomLoop = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .font(.title)
+                .padding()
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
+        }
     }
 }
 
